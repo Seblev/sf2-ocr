0976ed191d9d18e1df751c0928777424f4e5f024
Seblev <seblev@users.noreply.github.com>
2014-10-23 08:57:39 +0000

    plus loin sf2 : 5 observateur

---
+++ src/OC/PlatformBundle/Beta/BetaListener.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace OC\PlatformBundle\Beta;
+
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
+class BetaListener
+{
+
+    protected $betaHTML;
+    protected $endDate;
+
+    public function __construct(BetaHTML $betaHTML, $endDate)
+    {
+        $this->betaHTML = $betaHTML;
+        $this->endDate = new \Datetime($endDate);
+    }
+
+    public function processBeta(FilterResponseEvent $event)
+    {
+        if (!$event->isMasterRequest())
+        {
+            return;
+        }
+        $remainingDays = $this->endDate->diff(new \Datetime())->format('%d');
+        if ($remainingDays <= 0)
+        {
+            return;
+        }
+        $response = $this->betaHTML->displayBeta($event->getResponse(),
+                $remainingDays);
+        $event->setResponse($response);
+    }
+}
